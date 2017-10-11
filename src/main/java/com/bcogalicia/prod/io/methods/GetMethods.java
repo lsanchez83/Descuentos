3e98ce4c0824af1a268da22c6975e013dd84c91b
lsanchez <lsanchez@MJ138631.JUSTICIA.AR>
2017-10-11 16:18:15 -0300

    second commit

--- src/main/java/com/bcogalicia/prod/io/methods/GetMethods.java
+++ src/main/java/com/bcogalicia/prod/io/methods/GetMethods.java
@@ -1,8 +1,19 @@
 package com.bcogalicia.prod.io.methods;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 
 public class GetMethods {
+	
+	 public static String GetText(WebElement elemento) {
+		 return elemento.getAttribute("value");
+		 
+	 }
+	 
+	 public static String GetTextFromDDList(WebElement elemento){
+		 return new Select(elemento).getAllSelectedOptions().toString();
+	 }
+	
 
 }
